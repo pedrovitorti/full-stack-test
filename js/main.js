66689777b7a59a4b1cef81fe106f2fb48cbd05c3
pedrovitorti@gmail.com <pedrovitorti@gmail.com>
2019-09-01 16:34:13 -0300

    form validation javascript

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
-document.getElementById("nome").oninvalid = function() {  
-	this.setCustomValidity("");
-	if (!this.validity.valid) {
-		this.setCustomValidity("Insira seu nome completo.");
+function validar() {
+	var nome = meuFormulario.nome.value;
+	var email = meuFormulario.email.value;
+	var msg = meuFormulario.msg.value;
+
+	if (nome == "") {
+		document.getElementById("nome").oninvalid = function() {  
+			this.setCustomValidity("");
+			if (!this.validity.valid) {
+				this.setCustomValidity("Insira seu nome completo.");
+			}
+		};
+	
 	}
-};
-			
-document.getElementById("email").oninvalid = function() {  
-	this.setCustomValidity("");
-	if (!this.validity.valid) {
-		this.setCustomValidity("Insira um email válido.");
+
+	if (email == "" || email.indexOf('@') == /^[a-z0-9.]@[a-z0-9]?$/) {
+		document.getElementById("email").oninvalid = function() {  
+			this.setCustomValidity("");
+			if (!this.validity.valid) {
+				this.setCustomValidity("Insira um email válido.");
+			}
+		};
+		
 	}
-};
-			
-document.getElementById("msg").oninvalid = function() {  
-	this.setCustomValidity("");
-	if (!this.validity.valid) {
-		this.setCustomValidity("A mensagem deve conter pelo menos 4 palavras e 20 caracteres.");
+
+	if (msg == "") {
+		document.getElementById("msg").oninvalid = function() {  
+			this.setCustomValidity("");
+			if (!this.validity.valid) {
+				this.setCustomValidity("A mensagem deve conter pelo menos 4 palavras e 20 caracteres.");
+			}
+		};
+		
 	}
-};
-			
-			
+
+}
