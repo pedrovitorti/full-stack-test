7dad99759c2f8b08ccbeca75f136b90e16319081
pedrovitorti@gmail.com <pedrovitorti@gmail.com>
2019-09-02 08:23:17 -0300

    treating application backend

--- php/validar.php
+++ php/validar.php
@@ -1,16 +1,37 @@
 <?php 
     
-    // $nome = $_POST["nome"];
-    // $email = $_POST["email"];
-    // $msg = $_POST["msg"];
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $msg = $_POST["msg"];
     
-    // echo $nome;
-    // echo $email;
-    // echo $msg;
+    if(empty($nome)){
+    	header("Location: ../error.html");
+    }
 
-    $values = json_encode($_POST);
+    if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+    	header("Location: ../error.html");
+    }
+
+    if(empty($msg)){
+    	header("Location: ../error.html");
+    }
 
 
-    file_put_contents('../json/dados.json', $values, FILE_APPEND);
+    if(file_exists('../json/dados.json')) {
+    	$current_data = file_get_contents('dados.json');
+    	$array_data = json_decode($current_data,true);
+    	$extra = array (
+    		'nome' => $nome,
+    		'email' => $email,
+    		'msg' => $msg 
+    	);
+    	$array_data[] = $extra;
+    	$final_data = json_encode($array_data);
+    	echo $final_data;
+    	if(file_put_contents('../json/dados.json', $final_data)){
+    		header("Location: ../success.html"); 
+    	}
+    }
+
     
 ?>
